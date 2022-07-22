73ac471855857097689727e537464499fde90d3a
Shivank Joshi <89631936+SJ-1209@users.noreply.github.com>
2022-07-22 17:22:19 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo-1.png';
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={Logo} alt="logo" style={{ width: '220px', height: '95px' }} />
+    </Stack>
+    <Typography variant="h1" sx={{ fontSize: { lg: '20px', xs: '10px' } }} mt="30px" textAlign="center" pb="40px">Made with ❤️ by Shivank Joshi</Typography>
+  </Box>
+);
+
+export default Footer;
